cf9ec84e1acf66c2af9f76a22790745238e45421
fohisglory-dev <fohisglory@gmail.com>
2025-11-12 18:55:40 -0600

    Update injuries-nfl.js

--- api/injuries-nfl.js
+++ api/injuries-nfl.js
@@ -1,18 +1,34 @@
-// api/injuries-nfl.js
-
-const { scrapeLeague } = require("./_scrapeCBS");
+const axios = require("axios");
 
 module.exports = async (req, res) => {
   try {
-    const injuries = await scrapeLeague("nfl");
+    const url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/injuries";
+    const { data } = await axios.get(url, { timeout: 8000 });
+
+    const injuries = [];
+
+    for (const team of data.injuries) {
+      for (const player of team.injuries) {
+        injuries.push({
+          team: team.team.abbreviation,
+          teamName: team.team.displayName,
+          player: player.athlete.displayName,
+          position: player.athlete.position?.abbreviation || null,
+          injury: player.injury?.description || null,
+          status: player.injury?.status || null,
+          date: player.injury?.date || null,
+        });
+      }
+    }
 
     res.status(200).json({
-      league: "nfl",
+      league: "nba",
       updatedAt: new Date().toISOString(),
       count: injuries.length,
       injuries
     });
+
   } catch (err) {
-    res.status(500).json({ error: "NFL scraper failed", detail: String(err) });
+    res.status(500).json({ error: "NBA API failed", detail: String(err) });
   }
 };
